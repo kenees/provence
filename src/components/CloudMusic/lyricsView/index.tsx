ae92a2c4a04897c4782ea221b3a0cf00c2f7a465
kenevy <wc106916>
2020-08-09 17:30:28 +0800

    feat: add play music component

---
+++ src/components/CloudMusic/lyricsView/index.tsx
@@ -0,0 +1,16 @@
+/**
+ * @author kenevy
+ * @date  16:27
+ * @descript  ''
+ */
+
+import React from 'react';
+import styles from './index.module.scss';
+
+export default class LyricsView extends React.Component<{}, {}>{
+  render() {
+    return(
+      <div>2334</div>
+    )
+  }
+}
